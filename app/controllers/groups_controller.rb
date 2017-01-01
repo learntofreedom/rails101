15540ca7c3c12f5047ee8d6fd2cca4259d13fdc6
linchunlong <learntofreedom@gmail.com>
2017-01-01 14:28:54 +0800

    use simple_format helper to decorate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,9 @@ class GroupsController < ApplicationController
      def index
     @groups = Group.all
   end
-
+  def new
+   @group = Group.new
+  end
 
   def show
      @group = Group.find(params[:id])
